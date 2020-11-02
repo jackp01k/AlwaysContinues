6048272b2e17c60315e3a17bcd13c1fddaa69d5f
jackp01k <jackson.polk-1@ou.edu>
2020-11-02 16:50:08 -0600

    Finished for the day

--- src/Driver.java
+++ src/Driver.java
@@ -10,15 +10,16 @@ public class Driver {
 		//make a new JFrame object because that's what we're learning in zybooks
 		JFrame frame = new JFrame("My box");
 		
+
+		//from here on out, the program is written in nano. 
+		System.out.println("Written by nano!");
+		
 		frame.setSize(400, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		
-		//legal wildcard
-		ArrayList<Object> list1 = new ArrayList<Object>();
-		ArrayList<? super Number> list2 = list1;
-		
-		System.out.println("Written by nano!");
+		//Write something out just for another commit
+		System.out.println("Finished for the day");
 	}
 	
 	
